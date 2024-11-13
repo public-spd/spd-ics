3897e6538df7754adb749adb9242a4fe5f2bb217
spd-public <spd-public@example.com>
2024-11-13 01:54:10 +0100

    initial commit

---
+++ spd-de.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+import datetime
+import dateparser
+from bs4 import BeautifulSoup
+from ics import Calendar, Event
+
+OUTFILE = 'ics/spd-de.ics'
+TIMEZONE = 'DE'
+REGION = 'DE'
+URL = "https://www.spd.de/service/#m75572"
+
+
+def split_date(date, region=REGION, languages=[REGION.lower()]):
+    parts = [date]
+    if '/' in date:
+        parts = date.split('/')
+    for part in parts[:-1]:
+        yield dateparser.parse(part + " ".join(parts[-1].split(" ")[1:]), languages=languages, region=region)
+    yield dateparser.parse(parts[-1], languages=languages, region=region)
+
+
+def parse_time(time, region=REGION, languages=[REGION.lower()]):
+    if not time:
+        return None
+    if "Uhr" in time:
+        tzinfo = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
+        return datetime.time(hour=int(time.split("Uhr")[0]), tzinfo=tzinfo)
+    try:
+        return dateparser.parse(time, languages=languages, region=region, settings={
+                            'RETURN_AS_TIMEZONE_AWARE': True,
+                            'PREFER_DAY_OF_MONTH': 'current',
+                            'PREFER_DATES_FROM': 'current_period' }).time()
+    except:
+        return None
+
+
+def generate_events(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, features='lxml')
+
+    translation = {'events__item-date': 'date',
+                   'events__item-addition': 'time',
+                   'events__item-headline': 'name',
+                   'events__item-text': 'description',
+                   'events__item-cta-link': 'url'}
+
+    for event_element in soup.find_all(class_="events__item-wrapper"):
+        event = dict()
+
+        for fr,to in translation.items():
+            tmp = event_element.find(class_=fr)
+
+            if not tmp:
+                continue
+
+            if to == 'url':
+                event[to] = tmp.get('href')
+            elif to == 'time':
+                event[to] = parse_time(tmp.get_text())
+            else:
+                event[to] = tmp.get_text()
+        
+        for date in split_date(event['date']):
+            yield event|{'date': date}
+
+if __name__ == "__main__":
+    contained = set([])
+    calendar = Calendar()
+    if os.path.exists(OUTFILE):
+        with open(OUTFILE, 'r') as f:
+            calendar = Calendar("".join(f.readlines()))
+            for event in calendar.events:
+                contained.add("\t".join([str(event.name), str(event.url)]))
+
+    for event_dict in generate_events(URL):
+        begin = datetime.datetime.combine(event_dict['date'], event_dict['time']) if event_dict['time'] else event_dict['date']
+        event = Event(name=event_dict['name'],
+                      begin=begin.isoformat(),
+                      duration=datetime.timedelta(hours=1),
+                      description=event_dict['description'] if 'description' in event_dict else None,
+                      url=event_dict['url'])
+        if not event_dict['time']:
+            event.make_all_day()
+
+        if "\t".join([str(event.name), str(event.url)]) not in contained:
+            calendar.events.add(event)
+
+    with open(OUTFILE, 'w') as f:
+        f.writelines(calendar.serialize_iter())
---
+++ spd-dresden-de.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+import datetime
+import dateparser
+from bs4 import BeautifulSoup
+from ics import Calendar, Event
+
+OUTFILE = 'ics/spd-dresden-de.ics'
+TIMEZONE = 'DE'
+REGION = 'DE'
+URL = 'https://www.spd-dresden.de/termine/'
+
+
+def generate_events(url, region=REGION, languages=[REGION.lower()]):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, features='lxml')
+    tzinfo = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
+
+    for calendar_wrapper in soup.find_all(class_="ics-calendar-list-wrapper"):
+        for date_wrapper in calendar_wrapper.find_all(class_="ics-calendar-date-wrapper"):
+            date = date_wrapper.find(class_="ics-calendar-date").get_text()
+            for events in date_wrapper.find_all("dl", class_="events"):
+                times = [dt.get_text() for dt in events.find_all("dt", class_="time")]
+                events2 = events.find_all("dd", class_="event")
+                for time, event in zip(times, events2):
+                    title = event.find_all(class_="title")[0].get_text()
+                    try:
+                        desc = event.find_all(class_="eventdesc")[0].get_text()
+                    except IndexError:
+                        desc = None
+                    try:
+                        loc = event.find_all(class_="location")[0].get_text()
+                    except IndexError:
+                        loc = None
+                    begin = dateparser.parse(" ".join(date.split(" ")[1:]) + ", " + time.split("–")[0].strip(),
+                                             languages=languages,
+                                             region=region,
+                                             settings={
+                                                 'RETURN_AS_TIMEZONE_AWARE': True,
+                                                 'PREFER_DAY_OF_MONTH': 'current',
+                                                 'PREFER_DATES_FROM': 'current_period' })
+                    now = datetime.datetime.now(datetime.timezone.utc)
+                    if begin < now:
+                        begin = begin.replace(year=begin.year + 1)
+                    end = dateparser.parse(" ".join(date.split(" ")[1:]) + ", " + time.split("–")[1].strip(),
+                                           languages=languages,
+                                           region=region,
+                                           settings={
+                                               'RETURN_AS_TIMEZONE_AWARE': True,
+                                               'PREFER_DAY_OF_MONTH': 'current',
+                                               'PREFER_DATES_FROM': 'current_period' })
+                    if end < now:
+                        end = end.replace(year=end.year + 1)
+                    yield Event(name=title, description=desc, location=loc, begin=begin.isoformat(), end=end.isoformat())
+
+
+if __name__ == "__main__":
+    contained = set([])
+    calendar = Calendar()
+    if os.path.exists(OUTFILE):
+        with open(OUTFILE, 'r') as f:
+            calendar = Calendar("".join(f.readlines()))
+            for event in calendar.events:
+                contained.add("\t".join([str(event.name), str(event.url)]))
+
+    for event in generate_events(URL):
+        if "\t".join([str(event.name), str(event.url)]) not in contained:
+            calendar.events.add(event)
+
+    with open(OUTFILE, 'w') as f:
+        f.writelines(calendar.serialize_iter())
