b7d5652ee09d9b6776696caa8ec24c318f7dc4da
public-spd <public-spd@example.org>
2025-05-07 00:00:03 +0200

    fix: year

--- src/spd-dresden-de.py
+++ src/spd-dresden-de.py
@@ -50,7 +50,7 @@ def generate_events(url, region=REGION, languages=[REGION.lower()]):
                                            region=region,
                                            settings=SETTINGS)
                     if end < now:
-                        end = end.replace(year=end.year + 1)
+                        end = end.replace(year=now.year)
                     description = (desc + "\n") if desc else ""
                     description += ("Link: " + url) if url else ""
                     yield Event(name=title,
